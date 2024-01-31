8706df8b3d628eb6c21d32df3e05e2d442ebe808
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2024-01-31 17:17:07 +0000

    go ham with logs

--- cli/cmd/create.go
+++ cli/cmd/create.go
@@ -6,11 +6,13 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
@@ -56,21 +58,26 @@ func init() {
 }
 
 func runCreate(cmd *cobra.Command, args []string) {
+	slog.Debug("create command", slog.String("positional", args[0]))
 	printInto()
 
 	// Check node
 	err := checkNode()
 	if err != nil {
+		slog.Error("node check failed", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	slog.Debug("node check passed")
 
 	// Check yarn
 	err = checkYarn()
 	if err != nil {
+		slog.Error("yarn check failed", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	slog.Debug("yarn check passed")
 
 	// Target directory
 	tDir := "./redwood-app"
@@ -81,40 +88,50 @@ func runCreate(cmd *cobra.Command, args []string) {
 	}
 	vTDir, err := validateTargetDirectory(tDir, overwriteFlag)
 	if err != nil {
+		slog.Error("target directory validation failed", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	slog.Debug("target directory validation passed", slog.String("target", vTDir))
 	fmt.Printf("Target directory: %s\n", vTDir)
 
 	// TS or JS
 	useTS := typescriptFlag
 	if !cmd.Flags().Changed("typescript") && !yesFlag {
 		// TODO(jgmw): Prompt for TS or JS
+		slog.Debug("typescript flag unset, prompting")
 	}
+	slog.Debug("typescript flag", slog.Bool("typescript", useTS))
 	fmt.Printf("Use TypeScript: %t\n", useTS)
 
 	// Get the latest release
 	client := github.NewClient(nil)
-	if os.Getenv("RW_GITHUB_TOKEN") != "" {
-		client = client.WithAuthToken(os.Getenv("RW_GITHUB_TOKEN"))
+	token := os.Getenv("RW_GITHUB_TOKEN")
+	if token != "" {
+		slog.Debug("RW_GITHUB_TOKEN is set", slog.String("token", token[len(token)-4:]))
+		client = client.WithAuthToken(token)
 	}
 	release, _, err := client.Repositories.GetLatestRelease(context.Background(), GH_OWNER, GH_REPO)
 	if err != nil {
+		slog.Error("failed to get latest release", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	rTag := release.GetTagName()
-	fmt.Printf("%v\n", rTag)
+	slog.Debug("latest release", slog.String("tag", rTag))
+	fmt.Printf("Latest version: %s\n", rTag)
 
 	epochName := "arapaho"
 	if bighornFlag {
 		epochName = "bighorn"
 	}
+	slog.Debug("epoch choice", slog.String("name", epochName))
 
 	templateAssetName := epochName + "_js.zip"
 	if useTS {
 		templateAssetName = epochName + "_ts.zip"
 	}
+	slog.Debug("template asset name", slog.String("name", templateAssetName))
 
 	templateAssetId := int64(0)
 	for _, asset := range release.Assets {
@@ -124,10 +141,12 @@ func runCreate(cmd *cobra.Command, args []string) {
 	}
 
 	zipName := rTag + "_" + templateAssetName
+	slog.Debug("zip name", slog.String("name", zipName))
 
 	// Check if we already have the template cached
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
+		slog.Error("failed to get user home directory", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -139,17 +158,22 @@ func runCreate(cmd *cobra.Command, args []string) {
 		// Template is not cached
 	} else {
 		// Some other error
+		slog.Error("failed to check if template is cached", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
+	slog.Debug("template cache path", slog.String("path", cachePath))
+	slog.Debug("template cache status", slog.Bool("status", cachedTemplate))
 	fmt.Printf("Cached template: %t\n", cachedTemplate)
 
 	// Download template
 	if !cachedTemplate {
+		sTime := time.Now()
+
 		// TODO(jgmw): Consider downloading via a HTTP endpoint but could be less robust?
 		rc, _, err := client.Repositories.DownloadReleaseAsset(context.Background(), GH_OWNER, GH_REPO, templateAssetId, http.DefaultClient)
 		if err != nil {
+			slog.Error("failed to download template", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -157,11 +181,13 @@ func runCreate(cmd *cobra.Command, args []string) {
 
 		err = os.MkdirAll(filepath.Dir(cachePath), 0755)
 		if err != nil {
+			slog.Error("failed to create template cache directory", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
 		tf, err := os.Create(cachePath)
 		if err != nil {
+			slog.Error("failed to create template cache file", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -169,24 +195,31 @@ func runCreate(cmd *cobra.Command, args []string) {
 
 		_, err = io.Copy(tf, rc)
 		if err != nil {
+			slog.Error("failed to write template cache file", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		slog.Debug("template downloaded and saved", slog.Duration("duration", time.Since(sTime)))
 	}
 
 	// Create app from template
 	err = os.MkdirAll(vTDir, 0755)
 	if err != nil {
+		slog.Error("failed to create target directory", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	slog.Debug("target directory created", slog.String("path", vTDir))
 
 	// Unzip template into target directory
 	archive, err := zip.OpenReader(cachePath)
 	if err != nil {
-		panic(err)
+		slog.Error("failed to open template zip", slog.String("error", err.Error()))
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer archive.Close()
+	slog.Debug("template zip opened", slog.String("path", cachePath))
 
 	for _, f := range archive.File {
 		filePath := filepath.Join(vTDir, f.Name)
@@ -197,25 +230,35 @@ func runCreate(cmd *cobra.Command, args []string) {
 		}
 
 		if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-			panic(err)
+			slog.Error("failed to create directory for file in template", slog.String("error", err.Error()))
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 		if err != nil {
-			panic(err)
+			slog.Error("failed to create file in template", slog.String("error", err.Error()))
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		fileInArchive, err := f.Open()
 		if err != nil {
-			panic(err)
+			slog.Error("failed to open file in template", slog.String("error", err.Error()))
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		if _, err := io.Copy(dstFile, fileInArchive); err != nil {
-			panic(err)
+			slog.Error("failed to write file in template", slog.String("error", err.Error()))
+			fmt.Println(err)
+			os.Exit(1)
 		}
 
 		dstFile.Close()
 		fileInArchive.Close()
+
+		slog.Debug("template entry written", slog.String("path", filePath))
 	}
 
 	// Must rename the gitignore template file if it exists
@@ -223,10 +266,13 @@ func runCreate(cmd *cobra.Command, args []string) {
 	if _, err := os.Stat(gitignoreTemplatePath); err == nil {
 		err = os.Rename(filepath.Join(vTDir, "gitignore.template"), filepath.Join(vTDir, ".gitignore"))
 		if err != nil {
+			slog.Error("failed to rename gitignore template", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		slog.Debug("gitignore template renamed")
 	} else if !errors.Is(err, os.ErrNotExist) {
+		slog.Error("failed to check if gitignore template exists", slog.String("error", err.Error()))
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -234,16 +280,20 @@ func runCreate(cmd *cobra.Command, args []string) {
 	// Git
 	useGit := gitInitFlag
 	if !cmd.Flags().Changed("git-init") && !yesFlag {
+		slog.Debug("git flag unset, prompting")
 		// TODO(jgmw): Prompt for git
 	}
+	slog.Debug("git flag", slog.Bool("git", useGit))
 	fmt.Printf("Use Git: %t\n", useGit)
 
 	if useGit {
 		err = setupGit(cmd, vTDir)
 		if err != nil {
+			slog.Error("failed to setup git", slog.String("error", err.Error()))
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		slog.Debug("git setup complete")
 	}
 
 	// TODO(jgmw): Yarn install - maybe
@@ -279,8 +329,7 @@ func checkNode() error {
 func checkYarn() error {
 	yarns := which.All("yarn")
 	if len(yarns) == 0 {
-		fmt.Println("Yarn not found")
-		os.Exit(1)
+		return fmt.Errorf("yarn not found")
 	}
 	fmt.Println("Yarn found:")
 	for _, yarn := range yarns {
@@ -292,8 +341,7 @@ func checkYarn() error {
 	// TODO(jgmw): Execute yarn version check
 	yarnVer, err := exec.Command("yarn", "-v").Output()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 	fmt.Printf("Yarn version: %s\n", yarnVer)
 
@@ -305,13 +353,16 @@ func checkYarn() error {
 func setupGit(cmd *cobra.Command, vTDir string) error {
 	commitMsg := commitMessageFlag
 	if !cmd.Flags().Changed("commit-message") && !yesFlag {
+		slog.Debug("commit message flag unset, prompting")
 		// TODO(jgmw): Prompt for commit message
 	}
+	slog.Debug("commit message flag", slog.String("message", commitMsg))
 
 	// Perform git init
 	r, err := git.PlainInit(vTDir, false)
 	if err != nil {
 		if err == git.ErrRepositoryAlreadyExists {
+			slog.Warn("git repository already exists, skipping git init")
 			fmt.Println("Git repository already exists, skipping git init")
 			return nil
 		}
@@ -333,8 +384,9 @@ func setupGit(cmd *cobra.Command, vTDir string) error {
 	if err != nil {
 		return err
 	}
-
+	slog.Debug("initial commit complete", slog.String("message", commitMsg))
 	fmt.Printf("Commit message: %s\n", commitMsg)
+
 	return nil
 }
 
--- cli/main.go
+++ cli/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/redwoodjs/rw-cli/cli/cmd"
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/redwoodjs/rw-cli/cli/cmd"
+)
 
 // NOTE: These variables are set at compile time via ldflags by goreleaser
 var (
@@ -9,11 +18,99 @@ var (
 	date    = "unknown"
 )
 
+var logFile *os.File
+
+const (
+	RW_DIR_NAME = ".rw"
+)
+
 func main() {
 	// Forward the version information to the cmd package
 	cmd.BuildVersion = version
 	cmd.BuildCommit = commit
 	cmd.BuildDate = date
 
+	err := ensureDotRWExists()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = setupDebugLogger()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer teardownDebugLogger()
+
 	cmd.Execute()
+}
+
+func ensureDotRWExists() error {
+	uDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	dRWDir := filepath.Join(uDir, RW_DIR_NAME)
+	if _, err := os.Stat(dRWDir); os.IsNotExist(err) {
+		err = os.MkdirAll(dRWDir, 0755)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupDebugLogger() error {
+	uDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	// Open a file for logging
+	logFile, err = os.OpenFile(filepath.Join(uDir, RW_DIR_NAME, "debug.json"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Configure the logger
+	logger := slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+	slog.Debug("Start", slog.Time("at", time.Now()))
+
+	return nil
+}
+
+func teardownDebugLogger() error {
+	slog.Debug("Stop", slog.Time("at", time.Now()))
+
+	if logFile == nil {
+		return nil
+	}
+
+	// If the file gets too big (>1MB), delete it
+	fileInfo, err := logFile.Stat()
+	if err != nil {
+		return err
+	}
+	if fileInfo.Size() > 1_000_000 {
+		err = os.Remove(logFile.Name())
+		if err != nil {
+			return err
+		}
+	}
+
+	// Close the file
+	err = logFile.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
